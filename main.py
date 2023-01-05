f9adc4df92331dc4e0a435a941339f8ee502fc06
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2023-01-05 18:43:01 +0200

    fiksauksia, spagetit pois

--- main.py
+++ main.py
@@ -1,98 +1,40 @@
 import random
 
-class Kortti:
-    def __init__(self):
-        pass
+kortti = 0
+pakka = []
+poyta = []
 
-class Pakka:
-    def __init__(self):
-        pass
-
-class Poyta:
-    def __init__(self):
-        pass
-
-class Logiikka:
-    def __init__(self):
-        pass
-
-class Kortti:
-    def __init__(self, maa, arvo):
-        self.maa = maa
-        self.value = arvo
-
-    def show(self):         
-        print ("{} {}".format(self.maa, self.value))
+print(kortti)
 
 
-class Pakka:
-    def __init__(self):
-        self.kortit = []
-        self.kokoa()
+def teePakka():
+    a = 1
+    for m in ["Pata", "Risti", "Ruutu", "Hertta"]:
+        for i in range(1, 14):
+            pakka.append(m, a)
 
-    def kokoa(self):
-        for m in ["Pata", "Risti", "Ruutu", "Hertta"]:
-            for a in range(1, 14):
-                self.kortit.append(Kortti(m, a))
-
-    def show(self):
-        for c in self.kortit:
-            c.show()
-
-    def sekoitaPakka(self):
-        for i in range(len(self.kortit) -1, 0, -1):
-            r = random.randint(0, i)
-            self.kortit[i], self.kortit[r] = self.kortit[r], self.kortit[i]
-
-    def yksikortti(self):
-        nostetut = []
-        nostetut.insert(1, self.kortit.pop())
-        return nostetut
+teePakka()
+print(pakka)
 
 
-class Poyta(Pakka):
-    def __init__(self):
-        self.pelit = []
-        oikein = 0
-
-
-    def luoPoyta(self):
-        poyta = [[] for _ in range(9)]
-        for i in range(9):
-            jako = pakka.yksikortti()
-            poyta[i].append(jako)
-            pakka.kortit.pop(0)
-            print(i + 1)
-        a = (' ___' *  3 )
-        b = '   '.join('||||')
-        print('\n'.join((a, b, a, b, a, b, a, )))
-
-pakka = Pakka()
-pakka.sekoitaPakka()
-# pakka.show()
-poyta = Poyta()
-poyta.luoPoyta()
+def sekoitaPakka(pakka: list):
+    random.shuffle(pakka)
+    return pakka
 
 
 
-# 1. logiikka yhdelle korttipinolle, vertailu (isompi vai pienempi)
-# 2. laajennetaan yhdeksälle korttipinolle
-# 3. mihin pinoista kortti laitetaan --> vertailu
 
-
-class Logiikka:
+def pelaus():
     pino = []
     pino.append(Pakka.yksikortti(pakka))
 
-    vertailu = input("isompi vai pienempi?")
-    print ("Valinta:" + vertailu)
-
-
+    vertailu = str(input("isompi (+) vai pienempi (-)?"))
+    print("Valinta:" + vertailu)
 
     while True:
         oikein = 0
-        if vertailu == 'isompi':
-            if Pakka.yksikortti(pakka) > poyta[pino-1]:
+        if vertailu == '+':
+            if Pakka.yksikortti(pakka) > poyta[pino - 1]:
                 oikein += 1
                 print("Oikein!")
                 if oikein == 3:
@@ -100,10 +42,10 @@ class Logiikka:
                     oikein = 0
                 break
             else:
-                print("Väärin! Juo " + str(len(poyta[pino-1])+1))
+                print("Väärin! Juo " + str(len(poyta[pino - 1]) + 1))
                 break
-        elif vertailu == 'pienempi':
-            if Pakka.yksikortti(pakka) < poyta[pino-1][-1]:
+        elif vertailu == '-':
+            if Pakka.yksikortti(pakka) < poyta[pino - 1][-1]:
                 oikein += 1
                 print("Oikein!")
                 if oikein == 3:
@@ -111,5 +53,8 @@ class Logiikka:
                     oikein = 0
                 break
             else:
-                print("Väärin! Juo " + str(len(poyta[pino-1])+1))
+                print("Väärin! Juo " + str(len(poyta[pino - 1]) + 1))
                 break
+
+
+pelaus()
